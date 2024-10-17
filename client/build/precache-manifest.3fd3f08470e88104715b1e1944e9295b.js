d0b47667087a36318480c2882b456ff9253b082b
Ricokamau <ricokamau@gmail.com>
2024-10-17 17:44:37 +0300

    Initial commit

---
+++ client/build/precache-manifest.3fd3f08470e88104715b1e1944e9295b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa38d6b68efbc166be35f40d251c15f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01dad39faabfce5527a3",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "401e745065819688c86e",
+    "url": "/static/js/2.e7b1de21.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.e7b1de21.chunk.js.LICENSE"
+  },
+  {
+    "revision": "01dad39faabfce5527a3",
+    "url": "/static/js/main.fbf90640.chunk.js"
+  },
+  {
+    "revision": "4387b907a4d64303b5a2",
+    "url": "/static/js/runtime-main.fab42d54.js"
+  }
+]);
